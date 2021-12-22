2389c058f171641d5fc4d61db0d976a60d2f5a4f
Mirpas <36613633+DeimosDesktop@users.noreply.github.com>
2021-12-22 20:47:19 +0100

    Conector MySQL para usarlo en VB.NET

---
+++ Conector_BBDD_MySQL/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim comando As New MySqlConnection
+    Dim connectionString As String = "Server=localhost; database=visualb; UId=root; pwd="
+    Dim mysqlConnection As MySqlConnection = New MySqlConnection(connectionString)
+    Dim selección As MySqlCommand = New MySqlCommand
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            mysqlConnection.Open()
+            MsgBox("Conexión establecida con la BBDD correctamente!", MsgBoxStyle.Information, "Conexión establecida")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        selección.Connection = mysqlConnection
+        selección.CommandText = "SELECT * FROM Usuarios;"
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(selección)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+        mysqlConnection.Close()
+    End Sub
+End Class
